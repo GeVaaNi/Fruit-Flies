32db25b1a39026be862ddcf2401379204810c9a3
GeVaaNi <44638255+GeVaaNi@users.noreply.github.com>
2018-11-14 11:28:05 +0100

    Update Spray.java

--- Spray.java
+++ Spray.java
@@ -1,5 +1,4 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.util.concurrent.TimeUnit;
 /**
  * Write a description of class Spray here.
  * 
@@ -11,16 +10,12 @@ public class Spray extends Actor
     private GreenfootImage image1;
     private GreenfootImage image2;
     private GreenfootImage image3;
-    private GreenfootImage image4;
-    private GreenfootImage image5;
     public int Timer = 0;
     public Spray()
     {
         image1 = new GreenfootImage("Spray_idle.png");
         image2 = new GreenfootImage("Spray_stage1.png");
         image3 = new GreenfootImage("Spray_stage2.png");
-        image4 = new GreenfootImage("Spray_stage3.png");
-        image5 = new GreenfootImage("Spray_stage4.png");
         setImage(image1);
     }
     /**
@@ -29,26 +24,28 @@ public class Spray extends Actor
      */
     public void act() 
     {
-        Timer++;
-        if (Timer==500)
-        {
-            sprayTime();
-        }
-        
+        Timer++;    
+        sprayTime();
     }   
     /**
      * Set images to generate a spraying motion which kills the flies.
      * 
      */
-    public void sprayTime()
-    {
-        setImage(image2);
-        TimeUnit.SECONDS.sleep(1);
-        setImage(image3);
-        TimeUnit.SECONDS.sleep(1);
-        setImage(image4);
-        TimeUnit.SECONDS.sleep(7);
-        setImage(image5);
-        TimeUnit.SECONDS.sleep(3);
+    public void sprayTime(){
+        if (Timer == 500)
+        {
+            setImage(image2);
+        }
+        if (Timer == 530)
+        {
+            setImage(image3);
+        }
+        if (Timer == 830)
+        {
+            setImage(image1);
+            Timer = 0;
+        }
 }
 }
+
+
